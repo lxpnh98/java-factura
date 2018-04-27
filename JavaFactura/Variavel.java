6ae6ea0845613bacd9d4ae97288da08c94d129e1
Alexandre Pinho <lxpnh98@gmail.com>
2018-04-27 19:32:30 +0100

    Adicionar classes fundamentais.

---
+++ JavaFactura/Variavel.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Variavel here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Variavel implements Expressao
+{
+    // instance variables - replace the example below with your own
+    private double x;
+
+    /**
+     * Constructor for objects of class Variavel
+     */
+    public Variavel()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public double eval()
+    {
+        // put your code here
+        return x;
+    }
+}
