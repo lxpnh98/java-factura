17a07ccbb2575577de7d14264c82c06feb1677ba
Alexandre Pinho <lxpnh98@gmail.com>
2018-05-02 15:47:29 +0100

    Adicionar class Interface.

---
+++ JavaFactura/Interface.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Interface here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Interface
+{
+    // instance variables - replace the example below with your own
+    private Contribuinte contribuinte_atual;
+
+    /**
+     * Constructor for objects of class Interface
+     */
+    public Interface()
+    {
+        contribuinte_atual = null;
+    }
+}
